4cb1482df5ce81d1a3faaf95c6016c6d7aee309e
metanomial <benjamin@metanomial.com>
2020-11-10 13:16:17 -0800

    Use spaces

--- index.js
+++ index.js
@@ -5,21 +5,21 @@ const express = require('express')
 const app = express()
 
 app.post('/add_hash', (req, res) => {
-	// TODO
-	console.log(req)
+  // TODO
+  console.log(req)
 })
 
 app.get('/verify', (req, res) => {
-	// TODO
-	console.log(req)
+  // TODO
+  console.log(req)
 })
 
 app.use(express.static('static'))
 
 app.listen(PORT, () => {
-	console.log(`Serving on port ${PORT}`)
+  console.log(`Serving on port ${PORT}`)
 })
 
 setInterval(() => {
-	// TODO
+  // TODO
 }, INTERVAL)
