160a7010b600af09873c8b1e19d95a3b61cb6a8e
John David Pressman <jd@jdpressman.com>
2020-11-17 04:17:15 -0800

    Add body parsing to routing, set up test API endpoint

--- index.js
+++ index.js
@@ -3,13 +3,23 @@ const { PORT, INTERVAL } = process.env
 
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
 
-app.post('/add_hash', (req, res) => {
-  // TODO
-  console.log(req)
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.post('/api/add_hash', (req, res) => {
+    // TODO
+    if (req.body.h.length !== 64) {
+        res.send('{"error":"You didn\'t submit a sha256 hash."}');
+    }
+    else {
+        // TODO: This is where the addition to the Merkle tree goes
+        res.send('{"url":"test"}');
+    }
 })
 
-app.get('/verify', (req, res) => {
+app.get('/api/verify', (req, res) => {
   // TODO
   console.log(req)
 })
