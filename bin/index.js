0dcaf0c99ca0e633e6703dc7b4e2938571140314
John David Pressman <jd@jdpressman.com>
2020-11-20 00:24:19 -0800

    Add CLI and proof parsing to stamp subcommand

--- bin/index.js
+++ bin/index.js
@@ -4,43 +4,83 @@ var argv = require('minimist')(process.argv.slice(2));
 const crypto = require('crypto');
 const fs = require('fs');
 const fetch = require('node-fetch');
-const hash = crypto.createHash('sha256');
-hash.setEncoding('hex');
+const { Proof, _util: Hash } = require('@tzstamp/merkle')
 
 const subcommand = argv._[0];
 
-if (subcommand === "check")
-    {
-        const input = fs.createReadStream(argv._[1]);
-            digest = new Promise((resolve, reject) => {
-                input.on('error', err => reject(err));
-                input.on('data', chunk => hash.update(chunk));
-                input.on('end', () => resolve(hash.digest('hex')));
-            });
+const { createHash } = require('crypto')
 
-        digest.then(function (h) {
-            return fetch("http://localhost:8080/api/add_hash", {
-                method: 'POST',
-                body: JSON.stringify({h:h}),
-                headers: {
-                    "Content-type": "application/json"
-                }
+/**
+ * Asynchronously SHA-256 hash a read stream
+ */
+const sha256Async = stream => new Promise((resolve, reject) => {
+  if (stream.readableEnded) reject(new Error('Stream has ended'))
+  const hash = createHash('SHA256')
+  stream
+    .on('data', data => hash.update(data))
+    .on('end', () => resolve(new Uint8Array(hash.digest())))
+    .on('error', reject)
+})
+
+/**
+ * Asynchronously hash a file from read stream
+ */
+async function hashFile (path) {
+  const stream = fs.createReadStream(path)
+  try {
+    const hash = await sha256Async(stream) // returns Uint8Array
+    return hash
+  } catch (error) {
+    // Something went wrong with reading the file
+    // Do something with the error
+  }
+}
+
+async function handleVerify () {
+    // Determine what arguments we've been passed
+    sha256_p = /^[0-9a-fA-F]{64}$/;
+    http_p = /^https?:\/\//
+    const hash = argv._[1].match(sha256_p)
+          ? Hash.parse(argv._[1])
+          : await hashFile(argv._[1]);
+    const proof = argv._[2].match(http_p)
+        ? await fetch(argv._[2]).then(
+            function (_proof) {
+                return Proof.parse(_proof);
             })
+        : Proof.parse(fs.readFileSync(argv._[2]));
+    console.log(hash);
+    console.log(proof);
+}
+
+async function handleStamp () {
+    digest = hashFile(argv._[1])
+    digest.then(function (h) {
+        return fetch("http://localhost:8080/api/add_hash", {
+            method: 'POST',
+            body: JSON.stringify({h:Hash.stringify(h)}),
+            headers: {
+                "Content-type": "application/json"
+            }
+        })
             .then(res => res.json())
             .then(data => console.log(data))
             .catch(error => console.log(error));
-        })
-    }
-else if (subcommand === "stamp") {
-    console.log("Test");
+    })
 }
 
-else {
+function handleHelp () {
     console.log("Was not a recognized subcommand.");
 }
 
-/* const hashstr = 
-  digest.then(function (hashstr) {
-    return fulfilled;
-}); */
-
+switch (subcommand) {
+  case "verify":
+    handleVerify();
+    break;
+  case "stamp":
+    handleStamp();
+    break;
+  case "help":
+  default:
+    handleHelp();
+}
