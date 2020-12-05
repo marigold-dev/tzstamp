35b38e774d4b2d34551d2b513dff19d1f78ce0e2
John David Pressman <jd@jdpressman.com>
2020-12-05 05:06:33 -0800

    Add skeleton of hash injection into blockchain

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 require('dotenv-defaults').config()
 const {
   MerkleTree,
@@ -7,9 +8,44 @@ const {
     compare
   }
 } = require('@tzstamp/merkle')
+
 const express = require('express')
 
 const { PORT, INTERVAL } = process.env
+
+const config = JSON.parse(fs.readFileSync("config.json"))
+
+const { TezosToolkit } = require('@taquito/taquito')
+
+const { InMemorySigner } = require('@taquito/signer')
+
+const  { importKey } = require('@taquito/signer')
+
+const tezos = new TezosToolkit(config.TZNODE_RPC_URL);
+
+
+
+
+// TODO: Implement remote signer(?) branch
+if (config.FAUCET_KEY) {
+    importKey(
+        tezos,
+        config.FAUCET_KEY.email,
+        config.FAUCET_KEY.password,
+        config.FAUCET_KEY.mnemonic.join(' '),
+        config.FAUCET_KEY.secret
+    );
+}
+
+/*
+// 2020-12-03: Expect one method, bytes(), which takes a sha256; i.e. addHash()
+const tzstampContract = tezos.contract
+    .at(config.TZSTAMP_CONTRACT_ADDR)
+    .then(c => {
+        let methods = c.parameterSchema.ExtractSignatures();
+    })
+    .catch(error => console.log(`Error: ${error}`));
+*/
 
 let tree = new MerkleTree
 
@@ -21,7 +57,8 @@ express()
   .use(errorHandler)
   .listen(PORT, listenHandler)
 
-setInterval(notarizeTree, INTERVAL)
+// TODO: Set this to use config.CERTIFY_TREE_N_DAYS
+setInterval(notarizeTree, (1 * 60 * 1000))
 
 function postStamp (req, res) {
   if (!req.is('json'))
@@ -64,5 +101,15 @@ function listenHandler () {
 }
 
 function notarizeTree () {
-  // TODO
+    tezos.contract
+        .at(config.TZSTAMP_CONTRACT_ADDR)
+        .then(c => {
+            return c.methods.default("84714a61037b3b4fa539008681cbfa97c7256930279ff4b54bad7366521afc67").send()
+        })
+        .then(op => {
+            console.log(op)
+            return op.confirmation(3).then(() => op.hash);
+        })
+        .then(hash => console.log(`Operation injected: https://delphi.tzstats.com/${hash}`))
+        .catch(error => console.log(`Error: ${error.message}`));
 }
