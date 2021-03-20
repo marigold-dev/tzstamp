1f719f71044a911f830251c3e27979797a130c1d
Benjamin Herman <benjamin@metanomial.com>
2021-03-20 15:31:10 -0700

    Add byte array utilities

---
+++ src/bytes.ts
@@ -0,0 +1,15 @@
+// Uint8Array utilities
+
+/**
+ * Compare two byte arrays
+ */
+export function compare (a: Uint8Array, b: Uint8Array): boolean {
+  return a.length == b.length && a.every((val, idx) => val == b[idx])
+}
+
+/**
+ * Concatenate two byte arrays
+ */
+export function concat (a: Uint8Array, b: Uint8Array): Uint8Array {
+  return new Uint8Array([ ...a, ...b ])
+}
