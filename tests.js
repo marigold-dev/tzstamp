49635876843a04b3e2ffd2eaa89405d937850a32
Benjamin <benjamin@metanomial.com>
2021-04-18 02:54:33 -0700

    Write API tests

--- tests.js
+++ tests.js
@@ -1,77 +1,186 @@
-const assert = require('assert')
-const fs = require('fs')
+const crypto = require('crypto')
+const { spawn } = require('child_process')
 const fetch = require('node-fetch')
+const { Proof } = require('@tzstamp/proof')
+const { Hex } = require('@tzstamp/helpers')
+require('dotenv-defaults').config()
+
 const {
-  MerkleTree,
-  Proof,
-  _util: {
-    parse,
-    hash,
-    compare,
-    stringify
-  }
-} = require('@tzstamp/merkle')
+  BASE_URL,
+  RPC_URL
+} = process.env
 
-const hash1 = "55746df2109657ce123e2f676f35ce1845181999d534e2592a9590eb974d4223"
-const hash2 = "344f904b931e6033102e4235e592ea19f800ff3737ff3a18c47cfe63dbea2ed7"
-const hash3 = "3c06ddada5c6b4be8605f6396a16e9bf3a5cc730d0f34cbbf5d5707871ec5cce"
-const hash4 = "e94c5c36fae0474f5d4f19fcbe038dd98c069d4afa552f556e063ef6c9f95aa4"
-const hash5 = "a4e9de2410c9e7c3ac4c57bbc18beedc5935d5c8118e345a72baee00a9820b67"
+// UTF-8 text decoder
+const decoder = new TextDecoder('utf-8')
 
+// Start server in detached thread
+console.log('Starting server on detached thread')
+const server = spawn('node', [ 'index.js' ], { detached: true })
+let proofsFlag = false
 
-async function postHash (h) {
-    return await fetch("http://localhost:8080/api/stamp/", {
-        method: 'POST',
-        body: JSON.stringify({hash:h}),
-        headers: {
-            "Content-type": "application/json"
-        }
+// Capture server stdout
+server.stdout.on('data', data => {
+  const text = decoder.decode(data)
+  console.log('SERVER LOG:', text.slice(0, -1))
+
+  // Begin tests when the RESTful API is ready
+  if (text.startsWith('Serving on port')) {
+    runTests().catch(error => {
+      server.kill()
+      throw error
     })
-        .catch(error => console.log(error))
+  }
+
+  // Set proofs flag on successful publication
+  if (text.startsWith('Operation for')) {
+    proofsFlag = true
+  }
+})
+
+// Capture server stderr
+server.stderr.on('data', data => {
+  const text = decoder.decode(data)
+  console.log('SERVER ERROR:', text.slice(0, -1))
+})
+
+// Capture server closeing
+server.on('close', code => {
+  console.log('SERVER EXIT:', code)
+
+  // Server did not close gracefully
+  if (code != 0) {
+    process.exit(1)
+  }
+})
+
+// Tests mananger
+async function runTests () {
+
+  // Mock file hashes
+  const fileHashes = []
+  for (let i = 0; i < 20; ++i) {
+    const bytes = new Uint8Array(crypto.randomBytes(32))
+    fileHashes.push(bytes)
+  }
+
+  await fetchMissingProof(fileHashes[0])
+  const proofURLs = await postFiles(fileHashes)
+  await queryPendingProofs(proofURLs)
+  await proofsReady()
+  const proofs = await fetchProofs(proofURLs)
+  await verifyProofs(fileHashes, proofs)
+
+  console.log('All tests passed')
+  server.kill()
+  process.exit(0)
 }
 
-// Test 1: POSTing a hash and getting a 202 response
-
-async function test_1 () {
-    postHash(hash1)
-    .then(res => assert.equal(res.status, 202, "POSTed hash and got non-202 response"))
-    .catch(error => console.log(error))
+// Fetch a non-existant proof
+async function fetchMissingProof (fileHash) {
+  const proofId = Hex.stringify(fileHash)
+  const url = new URL(`/api/proof/${proofId}`, BASE_URL)
+  const response = await fetch(url, {
+    headers: { 'Accept': 'application/json' }
+  })
+  expect(response.status == 404, 'Fetching a non-existant proof did not yield a response status of 404')
+  console.log('Fetching non-existant proof yielded status 404')
 }
 
-// Test 2: POSTing a hash and GETing its proof
+// Stamp mock file hashes
+async function postFiles (fileHashes) {
+  const proofURLs = []
+  for (const fileHash of fileHashes) {
+    const url = new URL(`/api/stamp`, BASE_URL)
+    const response = await fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      },
+      body: JSON.stringify({
+        hash: Hex.stringify(fileHash)
+      })
+    })
+    expect(response.status == 202, 'Posting a file hash to be timestamped did not yield a response status of 202')
+    const json = await response.json()
+    proofURLs.push(json.url)
+  }
+  console.log(`Posted ${fileHashes.length} mock file hashes to be timestamped`)
+  return proofURLs
+}
 
-async function test_2 () {
-    posted = await postHash(hash2)
-    proof_url = (await posted.json()).url
-    proof = await fetch(proof_url).catch(error => console.log(error))
-    while (proof.status != 200) {
-        proof = await fetch(proof_url).catch(error => console.log(error))
+// Fetching pending proofs
+async function queryPendingProofs (proofURLs) {
+  for (const url of proofURLs) {
+    const response = await fetch(url, {
+      headers: { 'Accept': 'application/json' }
+    })
+    expect(response.status == 202, 'Fetching a pending proof did not yield a response status of 202')
+    const json = await response.json()
+    expect(json.status == 'Stamp pending', 'Pending proof query showed wrong status')
+    expect(json.url == url, 'Pending proof query showed contradictory proof URL')
+  }
+  console.log(`Queried ${proofURLs.length} pending proofs`)
+}
+
+// Wait for proofs to be ready
+function proofsReady () {
+  console.log('Waiting for next root publication...')
+  return new Promise(resolve => {
+    setInterval(() => {
+      if (proofsFlag) {
+        proofsFlag = false
+        resolve()
+      }
+    }, 100)
+  })
+}
+
+// Fetch proofs
+async function fetchProofs (proofURLs) {
+  const proofs = []
+  for (const url of proofURLs) {
+    const response = await fetch(url, {
+      headers: { 'Accept': 'application/json' }
+    })
+    expect(response.status == 200, 'Fetching a proof did not yield a response status of 200')
+    const text = await response.text()
+    try {
+      const proof = Proof.parse(text)
+      proofs.push(proof)
+    } catch (error) {
+      expect(false, `Could not parse proof: "${error.message}"`)
     }
-    assert.equal(proof.status, 200, "POSTed hash and couldn't get corresponding proof")
-    return proof
+  }
+  console.log(`Fetched and parsed ${proofURLs.length} proofs for mock file hashes`)
+  return proofs
 }
 
-
-// Test 3: Validating a proof
-
-async function test_3 (proof_obj) {
-    proof = Proof.parse(JSON.stringify(proof_obj))
-    assert.ok(
-        proof.verify(hash(parse(hash2)),
-                     parse("9043a5db1d3b5a8c2445ac77fd3f44c4ebb842a315cac3968ac7694c8845f298"))
-    )
-}
-
-async function run_tests () {
-    const proofs = fs.readdirSync("proofs")
-    for (const filename of proofs) {
-        fs.unlinkSync(`proofs/${filename}`)
+// Verify proofs
+async function verifyProofs (fileHashes, proofs) {
+  for (const index in fileHashes) {
+    const fileHash = fileHashes[index]
+    const proof = proofs[index]
+    const block = proof.derive(fileHash)
+    // console.log('DEBUG verifyProofs:', fileHash)
+    try {
+      const timestamp = await block.lookup(RPC_URL)
+    } catch (status) {
+      switch (status) {
+        case 404:
+          expect(false, `Could not verify proof. Block header could not be found.`)
+        default:
+          expect(false, `Could not verify proof. RPC returned status code "${status}"`)
+      }
     }
-    await test_1().catch(error => console.log(error))
-    proof_res = await test_2().catch(error => console.log(error))
-    proof = await proof_res.json()
-    await test_3(proof).catch(error => console.log(error))
-    console.log("Tests complete!")
+  }
+  console.log(`Verified ${fileHashes.length} proofs for mock file hashes`)
 }
 
-run_tests()
+function expect (expr, message) {
+  if (!expr) {
+    server.kill()
+    console.error(message)
+    process.exit(1)
+  }
+}
