d642b7fa96a02105664c0b9cabb7b53965787bf9
Benjamin <benjamin@metanomial.com>
2021-05-17 23:22:14 -0700

    Install dependencies - Add dependency `jtd` - Upgrade `@tzstamp/helpers` to 0.2.0 - Cleanup deps files

--- src/deps.deno.ts
+++ src/deps.deno.ts
@@ -1,4 +1,8 @@
+// Std
 export { createHash } from "https://deno.land/std@0.95.0/hash/mod.ts";
+export { assert } from "https://deno.land/std@0.95.0/testing/asserts.ts";
+
+// TzStamp helpers
 export {
   Base58,
   blake2b,
@@ -6,4 +10,8 @@ export {
   concat,
   Hex,
 } from "https://gitlab.com/tzstamp/helpers/-/raw/main/src/mod.ts";
-export { assert } from "https://deno.land/std@0.95.0/testing/asserts.ts";
+
+// JSON Typedef
+import { Schema, validate } from "https://deno.land/x/jtd@v0.1.0/mod.ts";
+export type { Schema };
+export { validate };
