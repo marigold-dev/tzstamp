b25d919f15515e3a56aac981abdf5e64ed5664e6
Benjamin <benjamin@metanomial.com>
2021-06-02 01:50:39 -0700

    Add test configurables

--- test/test.js
+++ test/test.js
@@ -4,9 +4,9 @@ const { tmpdir } = require('os')
 const { randomBytes } = require('crypto')
 const { Hex, blake2b } = require('@tzstamp/helpers')
 const { exec } = require('child_process')
-const { join } = require('path')
+const { join, resolve } = require('path')
 
-const SERVER_FLAG = '--servers http://localhost:8080'
+const SERVERS = process.env.SERVERS || 'http://localhost:8080'
 
 void async function () {
 
@@ -14,13 +14,17 @@ void async function () {
   // This will be a folder with random suffix,
   // in the format of "tzstamp-xxxxxx" in your OS's default temp directory
   console.log('Creating temporary directory')
-  const tempDir = await fs.mkdtemp(
-    join(
-      tmpdir(),
-      'tzstamp-'
-    )
-  )
-  console.log(`Created temporary directory at "${tempDir}"`)
+  let testDir
+  if (process.env.TESTDIR) {
+    await fs.mkdir(process.env.TESTDIR, {
+      recursive: true
+    })
+    testDir = resolve(process.env.TESTDIR)
+  } else {
+    const tempRoot = join(tmpdir(), 'tzstamp-')
+    testDir = await fs.mkdtemp(tempRoot)
+  }
+  console.log(`Created temporary directory at "${testDir}"`)
 
   // Create and store mock files
   // Files are named "fileN.dat" and stored in the temp directory
@@ -30,7 +34,7 @@ void async function () {
   for (let i = 0; i < 6; ++i) {
     const contents = Uint8Array.from(randomBytes(1024))
     const fileName = `file${i}.dat`
-    const filePath = path.join(tempDir, fileName)
+    const filePath = path.join(testDir, fileName)
 
     // File entries
     files.push({
@@ -46,22 +50,22 @@ void async function () {
 
   console.log('Stamping single file')
   await execTzstamp(
-    `stamp -d ${tempDir} ${SERVER_FLAG} ${files[0].path}`
+    `stamp -d ${testDir} -s ${SERVERS} ${files[0].path}`
   )
 
   console.log('Stamping single hash')
   await execTzstamp(
-    `stamp -d ${tempDir} ${SERVER_FLAG} ${files[1].hash}`
+    `stamp -d ${testDir} -s ${SERVERS} ${files[1].hash}`
   )
 
   console.log('Stamping multiple files and hash')
   await execTzstamp(
-    `stamp -d ${tempDir} ${SERVER_FLAG} ${files[2].path} ${files[3].path} ${files[4].hash} ${files[5].hash}`
+    `stamp -d ${testDir} -s ${SERVERS} ${files[2].path} ${files[3].path} ${files[4].hash} ${files[5].hash}`
   )
 
   console.log('Stamping and waiting for publication')
   await execTzstamp(
-    `stamp --wait -d ${tempDir} ${SERVER_FLAG} ${files[0].path}`
+    `stamp --wait -d ${testDir} -s ${SERVERS} ${files[0].path}`
   )
 
   console.log('Verifying file against proof')
@@ -71,7 +75,7 @@ void async function () {
 
   console.log('Verifying hash against proof')
   await execTzstamp(
-    `verify ${files[1].hash} ${join(tempDir, files[1].hash + '.proof.json')}`
+    `verify ${files[1].hash} ${join(testDir, files[1].hash + '.proof.json')}`
   )
 
   console.log('All tests passed')
