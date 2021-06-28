5b452a2e5a4ef055d90973ee190b8d4e79bdc3b4
Benjamin <benjamin@metanomial.com>
2021-06-28 04:15:25 -0700

    Remove unneeded custom error classes

--- errors.ts
+++ errors.ts
@@ -1,11 +1,4 @@
 /**
- * Invalid template error
- */
-export class InvalidTemplateError extends Error {
-  name = "InvalidTemplateError";
-}
-
-/**
  * Unsupported proof version error
  */
 export class UnsupportedVersionError extends Error {
@@ -31,27 +24,6 @@ export class UnsupportedVersionError extends Error {
  */
 export class MismatchedHashError extends Error {
   name = "MismatchedHashError";
-}
-
-/**
- * Unsupported operation error
- */
-export class UnsupportedOperationError extends Error {
-  name = "UnsupportedOperationError";
-
-  /**
-   * Operation label
-   */
-  operation: string;
-
-  /**
-   * @param operation Operation label
-   * @param message Optional error message
-   */
-  constructor(operation: string, message?: string) {
-    super(message);
-    this.operation = operation;
-  }
 }
 
 /**
