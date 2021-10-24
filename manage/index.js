1d9c354a04dd0b00f1b0eb122b67767dd5e6dd13
Felipe Cardozo <fraifelipe@gmail.com>
2021-10-24 05:42:09 -0300

    change the default network from giganode to ECAD Labs node

--- manage/index.js
+++ manage/index.js
@@ -6,7 +6,7 @@ const argv = parseArgs(
   process.argv,
   {
     'default': {
-      'node': 'https://mainnet-tezos.giganode.io/',
+      'node': 'https://mainnet.api.tez.ie',
       'indexer': 'https://api.better-call.dev/v1',
       'network': 'mainnet'
     },
